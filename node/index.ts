dcdad09a9f06e84d93a166f1dd5ee3b67381fbae
henriqueholtz <henrique_holtz@hotmail.com>
2022-05-05 05:54:30 -0300

    feat: create a config map with new volume to file (configmap to file), and added new endpoint "[GET] /configmap"

--- node/index.ts
+++ node/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+const fs = require('fs');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`Welcome! I'm ${process.env.name}. I'm ${process.env.age}`);
 });
 
-app.listen(3000, () => {
-  console.log('The application is listening on port 3000!');
+app.get('/configmap', (req, res) => {
+  let result;
+  try {
+    result = fs.readFileSync('files/readfile.txt', 'utf8');
+  } catch (err) {
+    console.error(err);
+    result = err;
+  }
+
+  res.send(`ConfigMap: ${result}`);
+});
+
+app.listen(port, () => {
+  console.log(`The application is listening on port ${port}!`);
 });
